53510578edeb475381fb7ddcaa0ba6124c40d73b
rafliyulistiawan <rafliyulistiawan@gmail.com>
2021-02-04 22:33:21 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'body': 'Poppins',
+      },
+      maxWidth: {
+        'card': '12rem',
+      },
+      minWidth: {
+        'card': '10rem',
+      },
+      gridTemplateColumns: {
+         'card': 'repeat(auto-fit, minmax(210px, 1fr))',
+      },
+      fill: theme => ({
+        'red': theme('colors.red.500'),
+        'gray': theme('colors.gray.400'),
+      })
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
